7754b3db9c335d763bc0518216d1f9a57ac8deaf
Lukas Heyer <Lukas.Heyer@bsi-software.com>
2023-12-14 13:56:48 +0100

    add alignment property to social follow

--- templates/email/properties.js
+++ templates/email/properties.js
@@ -252,6 +252,10 @@ module.exports = {
   // Elements using socialMediaIconHeight: social media follow
   socialMediaIconHeight: 20,
   
+  // Alignment of social media icons
+  // Elements using socialMediaIconAlignment: social media follow
+  socialMediaIconAlignment: 'center',
+
   // font weight of news title
   fontWeightNewsText: 'normal',
 
