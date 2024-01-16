7dde48aeda9f8a3a0bdfbd092b767d70d3c0e43a
Marleen Jattke <Marleen.Jattke@bsi-software.com>
2024-01-16 16:21:20 +0100

    Apply dom manipulation styles

---
+++ templates/email/content-elements/base/img-with-style/index.js
@@ -0,0 +1,15 @@
+const contentElement = require('@bsi-cx/design-standard-library-email/content-elements/base/image/prototype');
+
+module.exports = contentElement(
+  require('./template.twig'),
+  'img-with-style-wsFfey',
+  /*'Image',*/
+  'Bild',
+  '',
+  'img-with-style-part-image-Myqp3Q',
+  /*'Image',*/
+  'Bild',
+  'img-with-style-part-text-lVGDeP',
+  /*'Caption'*/
+  'Bildunterschrift'
+);
