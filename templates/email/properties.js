213de0c14f443c123de72f9cc21b77192f3ca9f6
LukasBSI <120456416+LukasBSI@users.noreply.github.com>
2025-12-03 11:30:02 +0100

    Update templates/email/properties.js

--- templates/email/properties.js
+++ templates/email/properties.js
@@ -340,7 +340,7 @@ module.exports = {
   // 'align-right'
   imageAlign: 'align-center',
 
-  // Sets the footer alignment.
+  // Sets the horizontal footer alignment.
   // 'align-footer-left'
   // 'align-footer-center'
   // 'align-footer-right'
